7f6a1986a7d2c0957e93fc85f647082c7ce13060
Allen <trippa002@gmail.com>
2026-09-14 10:25:05 -0500

    Update Formatter.cs with description comments

--- DrinksApp.Atizzle07/DrinkAPI/Services/Formatter.cs
+++ DrinksApp.Atizzle07/DrinkAPI/Services/Formatter.cs
@@ -1,9 +1,10 @@
-﻿namespace DrinksAPI.Services;
+namespace DrinksAPI.Services;
 
 public static class Formatter
 {
     public static string InstructionsFormat(string? instructions)
     {
+        // Formats drink instructions into a list format in the console output
         instructions = instructions ?? "";
         return instructions.Replace(". ", ".\n");
     }
